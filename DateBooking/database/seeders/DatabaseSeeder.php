5d4a3cc6558426e1301a66e29077082f9be1410a
AlbertoCard <alberto.cardenaszazueta@gmail.com>
2025-05-12 12:45:45 -0700

    create - factory tablas adicionales a servicio

--- DateBooking/database/seeders/DatabaseSeeder.php
+++ DateBooking/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Disponibilidad;
 use App\Models\Establecimiento;
 use App\Models\estbXusuario;
 use App\Models\Servicio;
@@ -9,6 +10,8 @@ use App\Models\User;
 use App\Models\Usuario;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
+use function Illuminate\Support\enum_value;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,8 +23,72 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         estbXusuario::factory(10);
+        Servicio::factory(20);
 
-        Servicio::factory();
+        //asignar disponibilidad a los servicios
+        $hoteles = Servicio::where('categoria', 'hotel')->get();
+        $eventos = Servicio::where('categoria', 'evento')->get();
+        $consultorios = Servicio::where('categoria', 'consultorio')->get();
+        $restaurantes = Servicio::where('categoria', 'restaurante')->get();
 
+        foreach ($hoteles as $hotel) {
+            Disponibilidad::factory(5)->create([
+                'id_servicio' => $hotel->id,
+                'fecha' => null,
+                'hora_inicio' => now()->addHours(rand(1, 12)),
+                'hora_fin' => now()->addHours(rand(13, 24)),
+                //valor time
+                'intervalos' => "00:00:00",
+                //valor enum
+                'dias' => enum_value('lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'),
+                'tipo' => 'recurrente',
+                'activo' => 1,
+            ]);
+        }
+
+        foreach ($eventos as $evento) {
+            Disponibilidad::factory(5)->create([
+                'id_servicio' => $evento->id,
+                'fecha' => now()->addDays(rand(1, 30)),
+                'hora_inicio' => now()->addHours(rand(1, 12)),
+                'hora_fin' => 'hora_inicio',
+                'intervalos' => "00:00:00",
+                'dias' => enum_value('lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'),
+                'tipo' => 'unica',
+                'activo' => 1,
+            ]);
+        }
+
+        foreach ($consultorios as $consultorio) {
+            Disponibilidad::factory(5)->create([
+                'id_servicio' => $consultorio->id,
+                'fecha' => null,
+                'hora_inicio' => now()->addHours(rand(1, 12)),
+                'hora_fin' => now()->addHours(rand(13, 24)),
+                //valor time
+                'intervalos' => "00:20:00",
+                //valor enum
+                'dias' => enum_value('lunes', 'martes', 'miercoles', 'jueves', 'viernes'),
+                'tipo' => 'recurrente',
+                'activo' => 1,
+            ]);
+        }
+
+        foreach ($restaurantes as $restaurante) {
+            Disponibilidad::factory(5)->create([
+                'id_servicio' => $restaurante->id,
+                'fecha' => null,
+                'hora_inicio' => now()->addHours(rand(8, 12)),
+                'hora_fin' => now()->addHours(rand(13, 20)),
+                //valor time
+                'intervalos' => "02:00:00",
+                //valor enum
+                'dias' => enum_value('lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado'),
+                'tipo' => 'recurrente',
+                'activo' => 1,
+            ]);
+        }
+
+        
     }
 }
