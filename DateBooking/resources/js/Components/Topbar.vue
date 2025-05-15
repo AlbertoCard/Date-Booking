63ed225d319d99994d704b6d9db633f4dc4a08a1
AlbertoCard <alberto.cardenaszazueta@gmail.com>
2025-05-15 10:14:47 -0700

    add - nuevo logo

--- DateBooking/resources/js/Components/Topbar.vue
+++ DateBooking/resources/js/Components/Topbar.vue
@@ -14,7 +14,13 @@
       </div>
   
       <div class="div_search">
-        <input type="text" v-model="searchText" class="search_input" placeholder="Comienza tu busqueda...">
+        <input
+          type="text"
+          v-model="searchText"
+          class="search_input"
+          placeholder="Comienza tu busqueda..."
+          @keyup.enter="redirectToSearch"
+        />
         <button @click="redirectToSearch" class="btnSearch">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 25 25" stroke-width="1.5" stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
